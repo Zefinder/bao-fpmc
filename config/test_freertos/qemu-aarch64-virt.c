9b1088de6462ef3353fa00632efaba6e994966a2
Zefinder <jakubiak@etud.insa-toulouse.fr>
2024-04-04 17:03:42 +0200

    Modified configuration and FreeRTOS

--- config/test_freertos/qemu-aarch64-virt.c
+++ config/test_freertos/qemu-aarch64-virt.c
@@ -7,47 +7,9 @@ struct config config = {
     
     CONFIG_HEADER
     
-    .vmlist_size = 2,
+    .vmlist_size = 1,
     .vmlist = {
-        { 
-            .image = {
-                .base_addr = 0x50000000,
-                .load_addr = VM_IMAGE_OFFSET(baremetal_image),
-                .size = VM_IMAGE_SIZE(baremetal_image)
-            },
-
-            .entry = 0x50000000,
-
-            .platform = {
-                .cpu_num = 1,
-                
-                .region_num = 1,
-                .regions =  (struct vm_mem_region[]) {
-                    {
-                        .base = 0x50000000,
-                        .size = 0x4000000 
-                    }
-                },
-
-                .dev_num = 1,
-                .devs =  (struct vm_dev_region[]) {
-                    {   
-                        /* Arch timer interrupt */
-                        .interrupt_num = 1,
-                        .interrupts = 
-                            (irqid_t[]) {27}                         
-                    }
-                },
-
-                .arch = {
-                    .gic = {
-                        .gicd_addr = 0x08000000,
-                        .gicr_addr = 0x080A0000,
-                    }
-                }
-            },
-        },
-                {
+        {
             .image = {
                 .base_addr = 0x0,
                 .load_addr = VM_IMAGE_OFFSET(freertos_image),
@@ -57,7 +19,7 @@ struct config config = {
             .entry = 0x0,
 
             .platform = {
-                .cpu_num = 1,
+                .cpu_num = 2,
                 
                 .region_num = 1,
                 .regions =  (struct vm_mem_region[]) {
@@ -67,16 +29,16 @@ struct config config = {
                     }
                 },
 
-            //    .ipc_num = 1,
-            //     .ipcs = (struct ipc[]) {
-            //         {
-            //             .base = 0x70000000,
-            //             .size = 0x00010000,
-            //             .shmem_id = 0,
-            //             .interrupt_num = 1,
-            //             .interrupts = (irqid_t[]) {52}
-            //         }
-            //     },
+               .ipc_num = 1,
+                .ipcs = (struct ipc[]) {
+                    {
+                        .base = 0x70000000,
+                        .size = 0x00010000,
+                        .shmem_id = 0,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {52}
+                    }
+                },
 
                 .dev_num = 2,
                 .devs =  (struct vm_dev_region[]) {
