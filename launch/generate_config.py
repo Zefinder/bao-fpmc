214f7bf77ddc5aa935009397079cc6ccc4507f42
Zefinder <jakubiak@etud.insa-toulouse.fr>
2024-04-10 16:33:20 +0200

    Continued configuration generation

--- launch/generate_config.py
+++ launch/generate_config.py
@@ -1,11 +1,18 @@
 # Imports
 import os
+from typing import Any
 
 # Dictionaries with constants
 image_information = {
     'zcu': {
         'baremetal': {
             'entry': 0x20000000,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0xff000000,
+                'va': 0xff000000,
+                'interrupt_number': 53
+            },
             'gic': {
                 'gicd': 0xf9010000,
                 'gicc': 0xf9020000
@@ -13,6 +20,12 @@ image_information = {
         },
         'freertos': {
             'entry': 0x0,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0xff000000,
+                'va': 0xff000000,
+                'interrupt_number': 53
+            },
             'gic': {
                 'gicd': 0xf9010000,
                 'gicc': 0xf9020000
@@ -20,6 +33,12 @@ image_information = {
         },
         'linux': {
             'entry': 0x00200000,
+            'ipc': 0xf0000000,
+            'uart': {
+                'pa': 0xff000000,
+                'va': 0xff000000,
+                'interrupt_number': 53
+            },
             'gic': {
                 'gicd': 0xf9010000,
                 'gicc': 0xf9020000
@@ -29,6 +48,12 @@ image_information = {
     'rpi4': {
         'baremetal': {
             'entry': 0x200000,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0xfe215000,
+                'va': 0xfe215000,
+                'interrupt_number': 125
+            },
             'gic': {
                 'gicd': 0xff841000,
                 'gicc': 0xff842000
@@ -36,6 +61,12 @@ image_information = {
         },
         'freertos': {
             'entry': 0x0,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0xfe215000,
+                'va': 0xff000000,
+                'interrupt_number': 125
+            },
             'gic': {
                 'gicd': 0xf9010000,
                 'gicc': 0xf9020000
@@ -43,13 +74,25 @@ image_information = {
         },
         'linux': {
             'entry': 0x20000000,
+            'ipc': 0xf0000000,
+            'uart': {
+                'pa': 0xfe215000,
+                'va': 0xfe215000,
+                'interrupt_number': 125
+            },
             'gic': {
                 'gicd': 0xff841000,
-                'gicc': 0xff842000
+                'gicc': 0xff842000,
             }
         },
         'zephyr': {
             'entry': 0x80000000,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0xfe215000,
+                'va': 0xfe215000,
+                'interrupt_number': 125
+            },
             'gic': {
                 'gicd': 0xff841000,
                 'gicc': 0xff842000
@@ -59,6 +102,12 @@ image_information = {
     'qemu-aarch64-virt': {
         'baremetal': {
             'entry': 0x50000000,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0x9000000,
+                'va': 0x9000000,
+                'interrupt_number': 33
+            },
             'gic': {
                 'gicd': 0x08000000,
                 'gicc': 0x080A0000
@@ -66,6 +115,12 @@ image_information = {
         },
         'freertos': {
             'entry': 0x0,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0x9000000,
+                'va': 0xff000000,
+                'interrupt_number': 33
+            },
             'gic': {
                 'gicd': 0xf9010000,
                 'gicc': 0xf9020000
@@ -73,6 +128,12 @@ image_information = {
         },
         'linux': {
             'entry': 0x60000000,
+            'ipc': 0xf0000000,
+            'uart': {
+                'pa': 0x9000000,
+                'va': 0x9000000,
+                'interrupt_number': 33
+            },
             'gic': {
                 'gicd': 0x08000000,
                 'gicc': 0x080A0000
@@ -80,6 +141,12 @@ image_information = {
         },
         'zephyr': {
             'entry': 0x80000000,
+            'ipc': 0x70000000,
+            'uart': {
+                'pa': 0x9000000,
+                'va': 0x9000000,
+                'interrupt_number': 33
+            },
             'gic': {
                 'gicd': 0x08000000,
                 'gicc': 0x080A0000
@@ -93,34 +160,30 @@ config_file_structure = '''#include <config.h>
 
 // List of used images
 {images:s}
-
 // Configuration struct
 struct config config = {{
     
     CONFIG_HEADER
     
-{body:s}
-}}
+{body:s}}}
 '''
 
 shared_memory_structure = '''    // Shared memory for IPC
     .shmemlist_size = {shmemlist_size:s},
     .shmemlist = (struct shmem[]) {{
-{shmemlist_template:s}
-    }},
+{shmemlist_template:s}    }},
 '''
 
 vm_list_structure = '''    // VM configuration
     .vmlist_size = {vmlist_size:d},
     .vmlist = {{
-{vm_config:s}
-    }},
+{vm_config:s}    }},
 '''
 
 platform_structure = '''            // Platform description
             .platform = {{
                 // CPU number
-                .cpu_num = {cpu_number:d},
+                .cpu_num = {cpu_number:s},
                 
 {region_description:s}
 
@@ -128,15 +191,25 @@ platform_structure = '''            // Platform description
 
 {device_description:s}
 
-{architecture_description:s}
-            }}
+{architecture_description:s}            }}
 '''
 
 region_structure = '''                // Memory description
                 .region_num = {region_num:d},
-                .regions = (struct vm_mem_regions[]) {{
-{region_template:s}
-                }},
+                .regions = (struct vm_mem_region[]) {{
+{region_template:s}                }},
+'''
+
+ipc_structure = '''                // IPC description
+                .ipc_num = {ipc_number:d},
+                .ipcs = (struct ipc[]) {{
+{ipc_template:s}                }},
+'''
+
+device_structure = '''                // Device description
+                .dev_num = {dev_num:d},
+                .devs = (struct vm_dev_region[]) {{
+{device_template:s}                }},
 '''
 
 # Template declarations
@@ -149,12 +222,45 @@ image_template = '''            // Image description
                 .size = VM_IMAGE_SIZE({image_name:s})
             }},
 '''
-entry_point_template = '            .entry = 0x{address:08x},'
+entry_point_template = '''            // Entry point
+            .entry = 0x{address:08x},
+'''
 region_template = '''                    {{
                         .base = 0x{base:08x},
                         .size = 0x{size:08x}
                     }},
 '''
+ipc_template = '''                    {{
+                        .base = 0x{base:08x},
+                        .size = 0x{size:08x},
+                        .shmem_id = {shmemid:d},
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {{{ipc_interrupt:d}}}
+                    }},
+'''
+device_template_no_intr = '''                    {{
+                        .pa = 0x{pa:08x},
+                        .va = 0x{va:08x},
+                        .size = 0x{size:08x},
+                    }},
+'''
+device_template_intr = '''                    {{
+                        .pa = 0x{pa:08x},
+                        .va = 0x{va:08x},
+                        .size = 0x{size:08x},
+                        .interrupt_num = {interrupt_num:d},
+                        .interrupts = (irqid_t[]) {{{dev_interrupts:s}}}
+                    }},
+'''
+timer_template = '''                    {{
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {{{timer_interrupt:d}}}
+                    }}
+'''
+
+ipc_interrupt_number = 52
+timer_interrupt_number = 27
+
 
 def shememlist_definition() -> list[int]:
     shmem_sizes = []
@@ -169,11 +275,11 @@ def shememlist_definition() -> list[int]:
     # Ask the size for any shared memory
     if shmemlist_size != 0:
         for shmem_index in range(0, shmemlist_size):
-            shmem_size_str = input(f'What is the size of the shared memory n°{shmem_index:d}? (type in hexadecimal without 0x) (by default 1000)\n')
+            shmem_size_str = input(f'What is the size of the shared memory n°{shmem_index:d}? (type in hexadecimal without 0x) (by default 10000)\n')
             try:
                 shmem_size = int(shmem_size_str, 16)
             except:
-                shmem_size = 0x1000
+                shmem_size = 0x10000
             
             shmem_sizes.append(shmem_size)
             
@@ -202,34 +308,152 @@ def declare_regions(entry_point: str, platform_name: str, region_number: int) ->
         
         # Else you ask for the base address (in hexa)
         else:
-            region_base_str = input(f'What is the base address for region n°{region_index:d}? (type in hexadecimal without 0x) (default 0)\n')
+            region_base_str = input(f'What is the base address for region n°{region_index:d}? (type in hexadecimal without 0x) (by default 0)\n')
             try:
                 region_base = max(int(region_base_str, 16), 0)
             except:
                 region_base = 0
         
         # Ask for region size
-        region_size_str = input(f'What is the size of region n°{region_index:d}? (type in hexadecimal without 0x) (default 0)\n')
+        region_size_str = input(f'What is the size of region n°{region_index:d}? (type in hexadecimal without 0x) (by default 0)\n')
         try:
             region_size = max(int(region_size_str, 16), 0)
         except:
             region_size = 0
         
+        # Append to regions
         regions += region_template.format(base=region_base, size=region_size)
+        
+        # Next one
         region_index += 1
     
     completed_region = region_structure.format(region_num=region_number, region_template=regions)
     return completed_region
 
 
-def declare_ipc(ipc_number: int) -> str:
+def declare_ipc(ipc_number: int, shmem_sizes: list[int], os_information: dict[str, Any]) -> str:
+    ipcs = ''
+    ipc_index = 0
+    
+    while ipc_index < ipc_number:
+        # Ask for which shared memory to use
+        shmemid = -1
+        while shmemid < 0:
+            shmemid_str = input(f'Which shared memory do you want to use for IPC n°{ipc_index}? (from 0 to {ipc_number -1})\n')
+            try:
+                shmemid = int(shmemid_str)
+            except:
+                shmemid = -1
+        
+        # Get information from constant dict and shared memory list
+        ipc_base = os_information['ipc']
+        ipc_size = shmem_sizes[shmemid]
+        
+        # Append to IPC
+        ipcs += ipc_template.format(base=ipc_base, size=ipc_size, shmemid=shmemid, ipc_interrupt=ipc_interrupt_number)
 
-    return ''
+        # Next one!
+        ipc_index += 1
+
+    completed_ipc = ipc_structure.format(ipc_number=ipc_number, ipc_template=ipcs)
+    return completed_ipc
 
 
-def declare_devices(device_number: int) -> str:
-
-    return '                // No devices'
+def declare_devices(device_number: int, os_information: dict[str, Any]) -> str:
+    devices = ''
+    device_index = 0
+    has_uart = 0
+    
+    while device_index < device_number:
+        pa = -1
+        va = -1
+        size = -1
+        interrupt_num = 0
+        interrupts = []
+        
+        # If there is no UART yet, ask if the device is a UART
+        if has_uart == 0:
+            confirmation = input('Is the device an UART? [Y/n]\n')
+            
+            # If it is an UART, then auto add everything
+            if confirmation.lower() != 'n':
+                has_uart = 1
+                pa = os_information['uart']['pa']
+                va = os_information['uart']['va']
+                
+                confirmation = input('Does this UART has access to the RX interrupt? [Y/n]\n')
+                
+                # If RX then we add the interrupt
+                if confirmation.lower() == 'n':
+                    interrupt_num = 0
+                else:
+                    interrupt_num = 1
+                    interrupts.append(str(os_information['uart']['interrupt_number']))
+                    
+        # Here if device is not UART or if UART already defined, pa should be -1
+        if pa == -1:
+            # Ask for physical address of device
+            while pa == -1: 
+                pa_str = input(f'What is the physical address of the device n°{device_index:d}? (type in hexadecimal without 0x)\n')
+                try:
+                    pa = max(int(pa_str, 16), -1)
+                except:
+                    pa = -1
+                    
+            # Ask for virtual address of device
+            while va == -1:
+                va_str = input(f'What is the virtual address of the device n°{device_index:d}? (type in hexadecimal without 0x)\n')
+                try:
+                    va = max(int(va_str, 16), -1)
+                except:
+                    va = -1
+                        
+            # Ask for number of interruptions
+            interrupt_num_str = input(f'What is the number of interruptions associated to the device? (by default 0)\n')
+            try:
+                interrupt_num = max(int(interrupt_num_str), 0)
+            except:
+                interrupt_num = 0
+            
+            # If any interruptions, ask their id
+            if interrupt_num > 0:
+                interrupt_index = 0
+                while interrupt_index < interrupt_num:
+                    interrupt_id = -1
+                    while interrupt_id == -1:
+                        interrupt_id_str = input(f'What is the interruption number of interruption n°{interrupt_index}?\n')
+                        try:
+                            interrupt_id = max(int(interrupt_id_str), -1)
+                        except:
+                            interrupt_id = -1
+                    
+                    interrupts.append(str(interrupt_id))
+                    interrupt_index += 1
+        
+        # Ask for size of device
+        size_str = input(f'What is the size of device n°{device_index}? (type in hexadecimal without 0x) (by default 10000)\n')
+        try:
+            size = max(int(size_str), 0)
+        except:
+            size = 0x10000
+        
+        # Append to devices
+        if interrupt_num == 0:
+            devices += device_template_no_intr.format(pa=pa, va=va, size=size)
+        else:
+            interrupts = ','.join(interrupts)
+            devices += device_template_intr.format(pa=pa, va=va, size=size, interrupt_num=interrupt_num, dev_interrupts=interrupts)
+        
+        # Increment device
+        device_index += 1
+    
+    # Add arch timer if wanted
+    confirmation = input('Do you want the arch timer? [Y/n]\n')
+    if confirmation.lower() != 'n':
+        devices += timer_template.format(timer_interrupt=timer_interrupt_number)
+    
+    completed_device = device_structure.format(dev_num=device_number, device_template=devices)
+    return completed_device
 
 
 def image_declaration(cpu_number: int, platform_name: str, shmem_sizes: list[int]) -> dict[int, dict[str, str]]:
@@ -311,6 +535,9 @@ def image_declaration(cpu_number: int, platform_name: str, shmem_sizes: list[int
         
         print(f'For the image n°{image_index:d} named {image["image_name"]:s}...')
         
+        # Set the number of CPU of the image
+        declared_config['cpu_number'] = image['cpu_number']
+        
         # Generate image string
         completed_image = generate_image_config(base_address=entry_point, image_name=image["image_name"])
         declared_config['image'] = completed_image
@@ -337,23 +564,23 @@ def image_declaration(cpu_number: int, platform_name: str, shmem_sizes: list[int
                 ipc_number = 1
             
             if ipc_number != 0:
-                completed_ipc = declare_ipc(ipc_number)
+                completed_ipc = declare_ipc(ipc_number=ipc_number, shmem_sizes=shmem_sizes, os_information=platform_information[image['configuration']])
                 declared_config['ipc'] = completed_ipc
 
         if 'ipc' not in declared_config:
             declared_config['ipc'] = '                // No IPC'
         
         # Ask for devices (do not count the arch timer)
-        device_number_str = input('How many devices do you want? (DO NOT INCLUDE THE ARCH TIMER) (by default 1)')
+        device_number_str = input('How many devices do you want? (DO NOT INCLUDE THE ARCH TIMER) (by default 1)\n')
         try: 
             device_number = max(int(device_number_str), 0)
         except:
             device_number = 1
         
-        completed_device = declare_devices(device_number)
+        completed_device = declare_devices(device_number=device_number, os_information=platform_information[image['configuration']])
         declared_config['devices'] = completed_device
     
-    print(declared_config['image'], declared_config['entry'], platform_structure.format(cpu_number=cpu_number, region_description=declared_config['regions'], ipc_description=declared_config['ipc'], device_description=declared_config['devices'], architecture_description='                // Architecture not done yet'), sep='\n')
+    print(declared_config['image'], declared_config['entry'], platform_structure.format(cpu_number=declared_config['cpu_number'], region_description=declared_config['regions'], ipc_description=declared_config['ipc'], device_description=declared_config['devices'], architecture_description='                // Architecture not done yet'), sep='\n')
         
     return declared_config
 
