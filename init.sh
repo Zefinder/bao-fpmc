e1a365e4d6c586389dcd32976c12117fdb0014db
Gabriel Aguiar <goaguiar99@gmail.com>
2024-04-25 02:24:38 -0300

    Added 'make install' and toolchain creation to init.sh

--- init.sh
+++ init.sh
@@ -11,10 +11,35 @@ then
     mkdir build-essentials
 fi
 
+# Creating toolchain directory
+if [ ! -d "toolchains" ]
+then
+    mkdir toolchains
+fi
+
 if [ ! -d "images" ]
 then
     mkdir images
 fi
+
+# Check if Make is installed
+if ! dpkg -s make &>/dev/null; then
+    echo "It seems that you don't have Make installed on your system. You will need Make to build dependencies."
+    echo "Do you want to install Make? [Y/n]"
+    read -r confirmation
+
+    # Convert confirmation to lower case
+    confirmation=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
+
+    # If empty or 'y', install Make
+    if [[ -z "$confirmation" ]] || [[ "$confirmation" == "y" ]]; then
+        echo "Installing Make..."
+        sudo apt install make
+    fi
+fi
+
+
+
 
 # Installing Qemu if user wants
 dpkg -s qemu-system >> /dev/null
