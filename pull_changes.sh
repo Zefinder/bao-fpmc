99209c8b0977f53265fd33f56d2d12ac70cf7ab8
Zefinder <jakubiak@insa-toulouse.fr>
2024-08-02 11:20:04 +0200

    Fixed pull changes script

--- pull_changes.sh
+++ pull_changes.sh
@@ -2,9 +2,9 @@
 set -e 
 
 # Basic git pull
-git pull
+git pull origin tests
 
 # Go to each submodule and pull
-(cd bao-hypervisor && git switch tests && git pull)
-(cd freertos-bao-fpmc && git switch tests && git pull)
-(cd baremetal-bao-fpmc && git switch tests && git pull)
+(cd bao-hypervisor && git switch tests && git pull origin tests)
+(cd freertos-bao-fpmc && git switch tests && git pull origin tests)
+(cd baremetal-bao-fpmc && git switch tests && git pull origin tests)
