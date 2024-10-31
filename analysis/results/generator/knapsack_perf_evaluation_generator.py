f5a60d6219fb2e454847b2b9e76adf210ec73cca
Zefinder <jakubiak@insa-toulouse.fr>
2024-10-31 14:05:50 +0100

    Finished knapsack generator

--- analysis/results/generator/knapsack_perf_evaluation_generator.py
+++ analysis/results/generator/knapsack_perf_evaluation_generator.py
@@ -18,10 +18,10 @@ random_delta_number = 5
 def analyse_data(result_log: log_results) -> list[tuple[int, int, float]]:
     results: list[tuple[int, int, float]] = []
     
-    # for _ in range(0, system_number * len(tasks_per_processor) * random_delta_number):
-    for _ in range(0, system_number * random_delta_number): # TODO Remove when real tests
-        line = result_log.read_line().split(',')
-        results.append((int(line[0]), int(line[1]), float(line[2])))
+    for task_number in tasks_per_processor:
+        for _ in range(0, system_number * random_delta_number):
+            line = result_log.read_line().split(',')
+            results.append((int(line[0]), int(line[1]), float(line[2]) / task_number))
     
     return results
 
@@ -42,8 +42,7 @@ def create_dict_t2d(results: list[tuple[int, int, float]]) -> dict[int, list[tup
             else:
                 pre_results[jobs_number][delta] = [time]
         else:
-            if jobs_number < 57: # TODO Remove when real tests
-                pre_results[jobs_number] = {delta: [time]}
+            pre_results[jobs_number] = {delta: [time]}
     
     for jobs_number in pre_results:
         result_dict[jobs_number] = []
@@ -53,69 +52,64 @@ def create_dict_t2d(results: list[tuple[int, int, float]]) -> dict[int, list[tup
     return result_dict
 
 
-def create_dict_d2t(): # Delta to task number
-    pass
-    
-
-def generate_knapsack_performance(resultsv1: dict[int, list[tuple[int, float]]], resultsv2: dict[int, list[tuple[int, float]]]):
-    jobs_numbers = list(resultsv1.keys())
+def generate_knapsack_performance(title: str, resultsv1: dict[int, list[tuple[int, float]]], resultsv2: dict[int, list[tuple[int, float]]]) -> None:
+    jobs_numbers = list(set(resultsv1.keys()) | set(resultsv2.keys()))
     jobs_numbers.sort()
-    fig, axs = plt.subplots(ceil(len(jobs_numbers) / 3), 3, figsize=(30, 50))
+    nb_lines = ceil(len(jobs_numbers) / 3)
+    _, axs = plt.subplots(nb_lines, 3, figsize=(30, 5 * nb_lines))
     
     for index in range(0, len(jobs_numbers)):
         job_number = jobs_numbers[index]
         plotx = floor(index / 3)
         ploty = index % 3
-        
-        x1 = []
-        y1 = []
-        for delta, time in resultsv1[job_number]:
-            x1.append(delta)
-            y1.append(time)
-            
-        x2 = []
-        y2 = []
-        for delta, time in resultsv2[job_number]:
-            x2.append(delta)
-            y2.append(time)
-        
         axs[plotx, ploty].set_title(f'Job number: {job_number:d}')
-        axs[plotx, ploty].plot(x1, y1)
-        axs[plotx, ploty].plot(x2, y2)
+        
+        # Only try to plot when job number is in the dict!
+        if job_number in resultsv1:
+            x1 = []
+            y1 = []
+            for delta, time in resultsv1[job_number]:
+                x1.append(delta)
+                y1.append(time)
+            axs[plotx, ploty].plot(x1, y1, 'o', label='classic DP')
+        
+        if job_number in resultsv2:
+            x2 = []
+            y2 = []
+            for delta, time in resultsv2[job_number]:
+                x2.append(delta)
+                y2.append(time)
+            axs[plotx, ploty].plot(x2, y2, 'o', color='tab:orange', label='improved DP')
+        
+        axs[plotx, ploty].legend(loc="upper left")
+        
     
     plt.subplots_adjust(bottom=0.1, 
                         top=0.9, 
                         wspace=0.4, 
-                        hspace=0.5
-                        )
-    plt.savefig(results_directory + 'test.png')
+                        hspace=0.5)
+    plt.savefig(results_directory + title + '.png')
+
 
 def generate() -> None:
     if assert_existing_result_files('knapsack_perf_evaluation.log', 'knapsackv2_perf_evaluation.log'):
-        # Read classic log
+        # Extract data from classical knapsack
         knapsack_log = log_file_class()
         knapsack_results = knapsack_log.create_result_file('knapsack_perf_evaluation.log')
         knapsack_data = analyse_data(knapsack_results)
         knapsack_results.close()
         knapsackv1_data = create_dict_t2d(knapsack_data)
-        keys = list(knapsackv1_data.keys())
-        keys.sort()
-        print(keys)
-
-        # Read knapsack log
+        
+        # Extract data from improved knapsack
         knapsackv2_log = log_file_class()
         knapsackv2_results = knapsackv2_log.create_result_file('knapsackv2_perf_evaluation.log')
         knapsack_data = analyse_data(knapsackv2_results)
         knapsackv2_results.close()
         knapsackv2_data = create_dict_t2d(knapsack_data)
-        keys = list(knapsackv2_data.keys())
-        keys.sort()
-        print(keys)
         
-        generate_knapsack_performance(knapsackv1_data, knapsackv2_data)
+        # Generate graph
+        generate_knapsack_performance('knapsack_perf_evaluation', knapsackv1_data, knapsackv2_data)
         
         
-
-
 if __name__ == '__main__':
     exit(1)
